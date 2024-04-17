4db27763277d6e8932702eeb3bfb144e3fc499ae
Cristanto <cristanto.sidharta@gmail.com>
2024-04-17 14:42:02 +0700

    day 8

---
+++ 8-remove space.js
@@ -0,0 +1,13 @@
+// CODING CHALLENGE 2024
+// 8 / 366
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+// const noSpace = (x) => x.replace(" ", "");
+
+// using split and join
+// const noSpace = (x) => x.split(" ").join("")
+
+// using replaceAll
+const noSpace = (x) => x.replaceAll(" ", "");
+
+console.log(noSpace("jfalsd ajsldkfj lk21"));

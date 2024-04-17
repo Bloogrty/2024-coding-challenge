c4170ca056a061ad808c6a36ca3f4779470e8ed7
Cristanto <cristanto.sidharta@gmail.com>
2024-04-17 14:07:17 +0700

    day 6 update

--- 6-string repeat.js
+++ 6-string repeat.js
@@ -2,12 +2,16 @@
 // 6 /366
 // https://www.codewars.com/kata/search/javascript?q=&r%5B%5D=-8&xids=completed&beta=false&order_by=popularity%20desc
 
-// functional declaration
-// function repeatStr(n, s) {
-//   return s.repeat(n);
-// }
+// functional declaration with for loop
+function repeatStr(n, s) {
+  str = "";
+  for (var i = 0; i < n; i++) {
+    str += s;
+  }
+  return str;
+}
 
-// functional expression
-const repeatStr = (n, s) => s.repeat(n);
+// functional expression with repeat
+// const repeatStr = (n, s) => s.repeat(n);
 
 console.log(repeatStr(4, "Hi"));

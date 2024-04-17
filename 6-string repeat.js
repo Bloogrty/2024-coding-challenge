e88c8cf54fbc9ac67fec8cf8f2fcf136dc7f05e2
Cristanto <cristanto.sidharta@gmail.com>
2024-04-17 14:04:18 +0700

    day 6

---
+++ 6-string repeat.js
@@ -0,0 +1,13 @@
+// CODING CHALLENGE 2024
+// 6 /366
+// https://www.codewars.com/kata/search/javascript?q=&r%5B%5D=-8&xids=completed&beta=false&order_by=popularity%20desc
+
+// functional declaration
+// function repeatStr(n, s) {
+//   return s.repeat(n);
+// }
+
+// functional expression
+const repeatStr = (n, s) => s.repeat(n);
+
+console.log(repeatStr(4, "Hi"));

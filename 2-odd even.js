4a04638194c0bf4227335cfc998f55ed590126b1
Cristanto <cristanto.sidharta@gmail.com>
2024-04-17 13:21:17 +0700

    day 2 - odd or even

---
+++ 2-odd even.js
@@ -0,0 +1,17 @@
+// CODING CHALLENGE 2024
+// 2 / 366
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/solutions/javascript
+
+// funtion dec
+// function evenOrOdd(number) {
+//   if (number % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// }
+
+// function expresion]
+const evenOrOdd = (number) => (number % 2 === 0 ? "Even" : "Odd");
+
+console.log(evenOrOdd());

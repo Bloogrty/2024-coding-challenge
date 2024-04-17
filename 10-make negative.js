c426e2262195212220164ce65a4d7e48f401d518
Cristanto <cristanto.sidharta@gmail.com>
2024-04-17 22:53:04 +0700

    saving

---
+++ 10-make negative.js
@@ -0,0 +1,10 @@
+// CODING CHALLENGE 2024
+// 10 / 366
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/solutions/javascript
+
+const makeNegative = (num) => (num > 0 ? -num : num);
+
+console.log(makeNegative(2));
+console.log(makeNegative(-7));
+console.log(makeNegative(-1));
+console.log(makeNegative(3));
